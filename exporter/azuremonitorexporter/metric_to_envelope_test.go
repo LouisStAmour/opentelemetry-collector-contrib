6e0a97a4fab80016d81ba4fdfafc422c91bb136d
Louis St-Amour <LouisStAmour@gmail.com>
2020-10-09 00:11:43 -0400

    WIP - Added exmaple metric

--- exporter/azuremonitorexporter/metric_to_envelope_test.go
+++ exporter/azuremonitorexporter/metric_to_envelope_test.go
@@ -13,3 +13,37 @@
 // limitations under the License.
 
 package azuremonitorexporter
+
+import "go.opentelemetry.io/collector/consumer/pdata"
+
+/*
+	The remainder of these methods are for building up test assets
+*/
+func getDefaultMetric() pdata.Metric {
+	tv := pdata.NewMetric()
+	tv.InitEmpty()
+	tv.SetName("test_name")
+	tv.SetDescription("test_description")
+	tv.SetUnit("1")
+	tv.IntGauge().InitEmpty()
+	tv.IntGauge().DataPoints().Resize(7)
+	for i := 0; i < tv.IntGauge().DataPoints().Len(); i++ {
+		dp := tv.IntGauge().DataPoints().At(i)
+		dp.LabelsMap().InitFromMap(map[string]string{
+			"k": "v",
+		})
+		dp.SetStartTime(pdata.TimestampUnixNano(1234567890))
+		dp.SetTimestamp(pdata.TimestampUnixNano(1234567890))
+		dp.SetValue(int64(-17))
+		dp.Exemplars().Resize(7)
+		for i := 0; i < dp.Exemplars().Len(); i++ {
+			ex := dp.Exemplars().At(i)
+			ex.SetTimestamp(pdata.TimestampUnixNano(1234567890))
+			ex.SetValue(int64(-17))
+			ex.FilteredLabels().InitFromMap(map[string]string{
+				"k": "v",
+			})
+		}
+	}
+	return tv
+}
